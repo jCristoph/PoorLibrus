6007f08c8ca47b8fc8ecde0d3b28870565591b7c
kowal189809 <mateus-kaja@wp.pl>
2020-11-15 23:42:22 +0100

    Dodanie systemu logowania

---
+++ DatabaseLibrary/Veryficate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLibrary
+{
+    class Veryficate
+    {
+        Base savedBase;
+        String login;
+        String password;
+        
+        Veryficate(Base base_, String login_, String password_)
+        {
+            savedBase = base_;
+            password = password_;
+            login = login_;
+        }
+
+    }
+}
